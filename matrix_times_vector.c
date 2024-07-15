5d61c06aecbd7f98444ab95bdeb4fa32cd1b1a18
seblful <alesha.chimba@gmail.com>
2024-07-15 15:18:14 +0300

    [ADD] M x V

---
+++ matrix_times_vector.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *matrixVectorMul(int **m, int *v) {
+
+};
+
+int main()
+{
+    int m1[2][2] = {{1, 2},
+                    {2, 4}};
+
+    int v1[2] = {1, 2};
+
+    return 0;
+}
