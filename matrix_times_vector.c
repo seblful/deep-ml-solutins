0f3030c6bb8f07163c7df48bea5bd8dc21601a9b
seblful <alesha.chimba@gmail.com>
2024-07-17 12:06:13 +0300

    [EDIT] change int to double

--- matrix_times_vector.c
+++ matrix_times_vector.c
@@ -4,13 +4,13 @@
 
 #include "utils.h"
 
-int *matrixVectorMul(int **m, int *v, int rows, int cols, int n)
+double *matrixVectorMul(double **m, double *v, int rows, int cols, int n)
 {
     // Assert if wrong size of matrix or vector
     assert(cols == n); // Vector size n should be the same as number of matrix cols."
 
     // Init empty matrix
-    int *result = calloc(sizeof(int), rows);
+    double *result = calloc(sizeof(double), rows);
 
     for (int i = 0; i < rows; i++)
     {
@@ -29,10 +29,10 @@ int main()
     int rows = 2, cols = 2, n = 2;
 
     // Init matrix
-    int **m1 = (int **)malloc(rows * sizeof(int *));
+    double **m1 = (double **)malloc(rows * sizeof(double *));
     for (int i = 0; i < cols; i++)
     {
-        m1[i] = (int *)malloc(cols * sizeof(int));
+        m1[i] = (double *)malloc(cols * sizeof(double));
     };
 
     // Fill matrix
@@ -42,23 +42,23 @@ int main()
     m1[1][1] = 4;
 
     printf("Matrix with %d rows and %d cols.\n", rows, cols);
-    printMatrix(m1, rows, cols);
+    printMatrix(m1, rows, cols, 0);
 
     // Init vector
-    int *v1 = (int *)malloc(n * sizeof(int));
+    double *v1 = (double *)malloc(n * sizeof(double));
 
     // Fill vector
     v1[0] = 1;
     v1[1] = 2;
     printf("Vector with size %d.\n", n);
-    printVector(v1, n);
+    printVector(v1, n, 0);
 
     // Matrix vector multiplication
     // Init result matrix
-    int *result;
+    double *result;
     result = matrixVectorMul(m1, v1, rows, cols, n);
     printf("Matrix vector multiplication result is:\n");
-    printVector(result, rows);
+    printVector(result, rows, 0);
 
     return 0;
 }
