755e120f351d42f4a13f480ddd809f53d7707b21
seblful <alesha.chimba@gmail.com>
2024-07-17 12:06:00 +0300

    [EDIT] add precision

--- utils.c
+++ utils.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printVector(int *v, int n)
+void printVector(double *v, int n, int precision)
 {
     for (int i = 0; i < n; i++)
     {
-        printf("%d ", v[i]);
+        printf("%.*f ", precision, v[i]);
     }
     printf("\n");
 };
 
-void printMatrix(int **m, int rows, int cols)
+void printMatrix(double **m, int rows, int cols, int precision)
 {
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
         {
-            printf("%d ", m[i][j]);
+            printf("%.*f ", precision, m[i][j]);
         }
         printf("\n");
     }
