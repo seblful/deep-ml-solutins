405a90c80feafa5e364a4aac2cc795c122955259
seblful <alesha.chimba@gmail.com>
2024-07-22 11:48:02 +0300

    [ADD] init commit

---
+++ svd_jacobi.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "utils.h"
+
+// Output struct
+typedef struct
+{
+    double **U;
+    double *E;
+    double **V;
+} DecompositionItem;
+
+int main()
+{
+    return 0;
+};
