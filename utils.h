b6a67e93045f0e2e622d596bddc3e5a1a4c2edde
seblful <alesha.chimba@gmail.com>
2024-07-23 11:00:55 +0300

    [EDIT] utils change

--- utils.h
+++ utils.h
@@ -7,4 +7,10 @@
 void printMatrix(double **m, int rows, int cols, int precision);
 void printVector(double *v, int n, int precision);
 
+double **allocateMatrix(int rows, int cols);
+void freeMatrix(int **matrix, int rows);
+
+void matrixMultiply(int **A, int rowsA, int colsA, int **B, int rowsB, int colsB, int **C);
+double **transposeMatrix(int **matrix, int rows, int cols);
+
 #endif
