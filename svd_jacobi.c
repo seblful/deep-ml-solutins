f019b736e2c876ae503461997a0e128940664358
seblful <alesha.chimba@gmail.com>
2024-07-23 11:19:40 +0300

    [EDIT] allocate, transpose, multiply matrices

--- svd_jacobi.c
+++ svd_jacobi.c
@@ -11,8 +11,26 @@ typedef struct
     double **V;
 } DecompositionItem;
 
-DecompositionItem decomposeMatrix(float **A)
+DecompositionItem decomposeMatrix(double **A, int rows, int cols)
 {
+    // Declaration and initialization of matrices
+    double **A_T, **A_T_A;
+    A_T = allocateMatrix(cols, rows);
+    A_T_A = allocateMatrix(cols, cols);
+
+    // Transpose matrix
+    A_T = transposeMatrix(A, rows, cols);
+    printf("Matrix A_T with %d rows and %d cols.\n", cols, rows);
+    printMatrix(A_T, cols, rows, 2);
+
+    // Multiply matrix
+    matrixMultiply(A_T, cols, rows, A, rows, cols, A_T_A);
+    printf("Matrix A_T_A with %d rows and %d cols.\n", cols, cols);
+    printMatrix(A_T_A, cols, cols, 2);
+
+    // Declaration of vector
+    double *E;
+
     return;
 };
 
@@ -25,12 +43,9 @@ int main()
     int rows = 2, cols = 2;
 
     // Init matrix
-    double **A = (double **)malloc(sizeof(double *) * rows);
+    double **A;
 
-    for (int i = 0; i < rows; i++)
-    {
-        A[i] = (double *)malloc(sizeof(double) * cols);
-    };
+    A = allocateMatrix(rows, cols);
 
     // Fill matrix
     A[0][0] = 2;
@@ -38,8 +53,12 @@ int main()
     A[1][0] = 1;
     A[1][1] = 2;
 
+    // Print matrix
+    printf("Matrix A with %d rows and %d cols.\n", rows, cols);
+    printMatrix(A, rows, cols, 2);
+
     // SVD
-    result = decomposeMatrix(A);
+    result = decomposeMatrix(A, rows, cols);
 
     return 0;
 };
