2d4016b0f23a869bc9203a2712586b87210571e8
seblful <alesha.chimba@gmail.com>
2024-07-23 15:08:33 +0300

    [EDIT] calculate A_prime

--- svd_jacobi.c
+++ svd_jacobi.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "utils.h"
 
@@ -14,9 +15,19 @@ typedef struct
 DecompositionItem decomposeMatrix(double **A, int rows, int cols)
 {
     // Declaration and initialization of matrices
-    double **A_T, **A_T_A;
+    double **A_T, **A_T_A, **J, **J_T, **A_temp, **A_prime;
     A_T = allocateMatrix(cols, rows);
-    A_T_A = allocateMatrix(cols, cols);
+    A_T_A = allocateMatrix(rows, rows);
+    J = allocateMatrix(rows, cols);
+    J_T = allocateMatrix(cols, rows);
+    A_temp = allocateMatrix(cols, rows);
+    A_prime = allocateMatrix(cols, cols);
+
+    // Declaration of vector
+    double *E;
+
+    // Declaration of numbers
+    double theta;
 
     // Transpose matrix
     A_T = transposeMatrix(A, rows, cols);
@@ -24,12 +35,32 @@ DecompositionItem decomposeMatrix(double **A, int rows, int cols)
     printMatrix(A_T, cols, rows, 2);
 
     // Multiply matrix
-    matrixMultiply(A_T, cols, rows, A, rows, cols, A_T_A);
-    printf("Matrix A_T_A with %d rows and %d cols.\n", cols, cols);
+    matrixMultiply(A, cols, rows, A_T, rows, cols, A_T_A);
+    printf("Matrix A_T_A with %d rows and %d cols.\n", rows, rows);
     printMatrix(A_T_A, cols, cols, 2);
 
-    // Declaration of vector
-    double *E;
+    // Calculate theta
+    theta = 0.5 * atan2(2 * A_T_A[0][1], A_T_A[0][0] - A_T_A[1][1]);
+    printf("Theta is %f.\n", theta);
+
+    // Fill J
+    J[0][0] = cos(theta);
+    J[0][1] = -sin(theta);
+    J[1][0] = sin(theta);
+    J[1][1] = cos(theta);
+
+    // Print J
+    printf("Matrix J with %d rows and %d cols.\n", rows, cols);
+    printMatrix(J, rows, cols, 3);
+
+    // Transpose K
+    J_T = transposeMatrix(J, rows, cols);
+
+    // Calculate A_prime
+    matrixMultiply(J_T, cols, rows, A_T_A, rows, rows, A_temp);
+    matrixMultiply(A_temp, cols, rows, J, rows, cols, A_prime);
+    printf("Matrix A_prime with %d rows and %d cols.\n", cols, cols);
+    printMatrix(A_prime, cols, cols, 2);
 
     return;
 };
