923305cb55844497eb899601e63f8932622bf177
seblful <alesha.chimba@gmail.com>
2024-07-23 10:59:41 +0300

    [EDIT] allocate and free matrix

--- utils.c
+++ utils.c
@@ -22,3 +22,24 @@ void printMatrix(double **m, int rows, int cols, int precision)
     }
     return;
 };
+
+// Function to allocate a 2D array
+double **allocateMatrix(int rows, int cols)
+{
+    double **matrix = (double **)malloc(rows * sizeof(double *));
+    for (int i = 0; i < rows; i++)
+    {
+        matrix[i] = (double *)malloc(cols * sizeof(double));
+    }
+    return matrix;
+}
+
+// Function to free a 2D array
+void freeMatrix(int **matrix, int rows)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
