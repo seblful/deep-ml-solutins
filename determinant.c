90772d1b904ca2d1c3cde3f78c63417f4b1511a7
seblful <alesha.chimba@gmail.com>
2024-07-26 12:14:25 +0300

    [EDIT] calculateDeterminant and createMinor

--- determinant.c
+++ determinant.c
@@ -3,26 +3,73 @@
 
 #include "utils.h"
 
-double calculateDeterminant(double **A, int rows, int cols)
+void createMinor(double **A, double **minor, int rows, int cols, int skipRow, int skipCol)
 {
-    // Create minor matrix
-    double **minor = allocateMatrix(rows - 1, cols - 1);
+    int minorRow = 0, minorCol = 0;
 
-    // Iterating through matrix
     for (int i = 0; i < rows; i++)
     {
+        if (i == skipRow)
+            continue;
+        minorCol = 0;
         for (int j = 0; j < cols; j++)
         {
-            ;
+            if (j == skipCol)
+                continue;
+            minor[minorRow][minorCol] = A[i][j];
+            minorCol++;
         }
+        minorRow++;
     }
-    return 0;
-};
+}
+
+double calculateDeterminant(double **A, int rows, int cols)
+{
+    if (rows != cols)
+    {
+        printf("Error: Matrix must be square to calculate determinant.\n");
+        return 0;
+    }
+
+    // Base case for 2x2 matrix
+    if (rows == 2)
+    {
+        return A[0][0] * A[1][1] - A[0][1] * A[1][0];
+    }
+
+    double det = 0;
+    int sign = 1;
+    double **minor = allocateMatrix(rows - 1, cols - 1);
+
+    for (int j = 0; j < cols; j++)
+    {
+        createMinor(A, minor, rows, cols, 0, j);
+
+        // Print minor
+        printf("\nMinor matrix for element A[0][%d]:\n", j);
+        printMatrix(minor, rows - 1, cols - 1, 2);
+
+        det += sign * A[0][j] * calculateDeterminant(minor, rows - 1, cols - 1);
+        sign = -sign;
+    }
+
+    // Free the allocated memory for minor matrix
+    for (int i = 0; i < rows - 1; i++)
+    {
+        free(minor[i]);
+    }
+    free(minor);
+
+    return det;
+}
 
 int main()
 {
     // Declaration of rows and cols in A matrix
     int rows = 4, cols = 4;
+
+    // Declaration of determinant result
+    double determinant;
 
     // Init matrix
     double **A;
@@ -42,6 +89,10 @@ int main()
     // Print matrix
     printf("Matrix A with %d rows and %d cols.\n", rows, cols);
     printMatrix(A, rows, cols, 0);
+
+    // Calculate dererminant
+    determinant = calculateDeterminant(A, rows, cols);
+    printf("Determinant of matrix is %f\n", determinant);
 
     return 0;
 }
