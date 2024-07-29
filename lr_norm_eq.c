75a2ef88010d75722eaafb6355e96919e12e1f4d
seblful <alesha.chimba@gmail.com>
2024-07-29 12:15:19 +0300

    [EDIT] init values

--- lr_norm_eq.c
+++ lr_norm_eq.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "utils.h"
+
+double *calculateTheta(double **X, double *y, double *theta) { return; };
+
 int main()
 {
+    // Declaration of rows and cols in A matrix
+    int rows = 3, cols = 2;
+
+    // Init X
+    double **X = allocateMatrix(rows, cols);
+
+    // Fill X
+    X[0][0] = 1;
+    X[0][1] = 1;
+    X[1][0] = 1;
+    X[1][1] = 2;
+    X[2][0] = 1;
+    X[2][1] = 3;
+
+    // Print X
+    printf("Matrix X with %d rows and %d cols.\n", rows, cols);
+    printMatrix(X, rows, cols, 0);
+
+    // Init y and result
+    double *y = (double *)malloc(sizeof(double) * rows);
+    double *theta = (double *)malloc(sizeof(double) * cols);
+
+    // Fill y
+    y[0] = 1;
+    y[1] = 2;
+    y[2] = 3;
+
+    // Print y
+    printf("Vector y with size %d.\n", rows);
+    printVector(y, cols, 0);
+
+    // Calculate theta
+    theta = calculateTheta(X, y, theta);
+
     return 0;
 }
