e3b2eede07734d8d0c8f535c1ea2b46bd4565dce
seblful <alesha.chimba@gmail.com>
2024-07-30 11:35:48 +0300

    [EDIT] init vars

--- lr_grad_desc.c
+++ lr_grad_desc.c
@@ -5,5 +5,39 @@
 
 int main()
 {
+    // Declaration of rows and cols in X matrix
+    int rows = 3, cols = 2;
+
+    // Init X
+    double **X = allocateMatrix(rows, cols);
+
+    // Fill X
+    X[0][0] = 1;
+    X[0][1] = 1;
+    X[1][0] = 1;
+    X[1][1] = 2;
+    X[2][0] = 1;
+    X[2][1] = 3;
+
+    // Print X
+    printf("Matrix X with %d rows and %d cols.\n", rows, cols);
+    printMatrix(X, rows, cols, 0);
+
+    // Init y and result
+    double *y = (double *)malloc(sizeof(double) * rows);
+    double *result;
+
+    // Fill y
+    y[0] = 1;
+    y[1] = 2;
+    y[2] = 3;
+
+    // Print y
+    printf("Vector y with size %d.\n", rows);
+    printVector(y, rows, 0);
+
+    // Init numbers
+    double alpha = 0.01;
+    int iterations = 1000;
     return 0;
 };
