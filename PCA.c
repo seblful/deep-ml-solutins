314d0a29a66ce0ca44b8317abf7bb222a7bef027
seblful <alesha.chimba@gmail.com>
2024-09-10 12:30:03 +0300

    [EDIT] create covariance matrix

--- PCA.c
+++ PCA.c
@@ -3,6 +3,67 @@
 
 #include "utils.h"
 
+double calculateMean(double *v, int size)
+{
+    // Declare sum
+    double sum = 0;
+
+    // Iterating through vector and calculate mean
+    for (int i = 0; i < size; i++)
+    {
+        sum += v[i];
+    }
+
+    return sum / size;
+};
+
+double **createCovarianceMatrix(double **m, int f, int r)
+{
+    // Init arrays
+    double *means = malloc(sizeof(double) * f);
+
+    // Init result matrix
+    double **result = (double **)malloc(sizeof(double *) * f);
+    for (int i = 0; i < f; i++)
+    {
+        result[i] = (double *)calloc(sizeof(double), f);
+    };
+
+    // Calculate means
+    for (int i = 0; i < f; i++)
+    {
+        means[i] = calculateMean(m[i], r);
+    };
+
+    // Fill matrix
+
+    for (int i = 0; i < f; i++)
+    {
+        for (int j = 0; j < f; j++)
+        {
+            double sum = 0;
+            for (int k = 0; k < r; k++)
+            {
+                sum += (m[i][k] - means[i]) * (m[j][k] - means[j]);
+            }
+            result[i][j] = sum / (r - 1);
+        }
+    };
+
+    // Free memory
+    free(means);
+
+    return result;
+};
+
+double **performPCA(double **data, int rows, int cols)
+{
+    // Init covariance matrix;
+    double **covMatrix = createCovarianceMatrix(data, rows, cols);
+    printf("Covariance matrix covMatrix with %d rows and %d cols.\n", cols, cols);
+    printMatrix(covMatrix, cols, cols, 1);
+}
+
 int main()
 {
     // Number of principal of components
@@ -24,5 +85,8 @@ int main()
 
     // Print data
     printf("Matrix data with %d rows and %d cols.\n", rows, cols);
-    printMatrix(data, rows, cols, 0);
+    printMatrix(data, rows, cols, 1);
+
+    // Perform PCA
+    double **result = performPCA(data, rows, cols);
 };
