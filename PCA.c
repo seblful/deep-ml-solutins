aff811b5d888730504c6b53928a6760a48eb366d
seblful <alesha.chimba@gmail.com>
2024-09-11 09:00:09 +0300

    [EDIT] find eigenvectors and eigenvalues

--- PCA.c
+++ PCA.c
@@ -1,7 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "utils.h"
+
+typedef struct
+{
+    double *eigenValues;
+    double **eigenVectors;
+} Eigen;
 
 double calculateMean(double *v, int size)
 {
@@ -56,12 +63,72 @@ double **createCovarianceMatrix(double **m, int f, int r)
     return result;
 };
 
+Eigen findEig(double **matrix, int size)
+{
+    // Allocate memory for eigenvalue vector and eigenvectors matrix
+    double *eigenvalues = (double *)malloc(size * sizeof(double));
+    double **eigenvectors = allocateMatrix(size, size);
+
+    double a = matrix[0][0];
+    double b = matrix[0][1];
+    double c = matrix[1][0];
+    double d = matrix[1][1];
+
+    // Calculate the trace and determinant of the matrix
+    double trace = a + d;
+    double determinant = a * d - b * c;
+
+    // Calculate the eigenvalues using the quadratic formula
+    double discriminant = sqrt(trace * trace - 4 * determinant);
+    eigenvalues[0] = (trace + discriminant) / 2;
+    eigenvalues[1] = (trace - discriminant) / 2;
+
+    // Print eigenvalues
+    printf("Vector eigenvalues with size %d.\n", size);
+    printVector(eigenvalues, size, 2);
+
+    // Calculate the eigenvectors
+    for (int i = 0; i < 2; i++)
+    {
+        double lambda = eigenvalues[i];
+        if (b != 0)
+        {
+            eigenvectors[i][0] = lambda - d;
+            eigenvectors[i][1] = b;
+        }
+        else if (c != 0)
+        {
+            eigenvectors[i][0] = c;
+            eigenvectors[i][1] = lambda - a;
+        }
+        else
+        {
+            eigenvectors[i][0] = 1;
+            eigenvectors[i][1] = 0;
+        }
+    }
+
+    // Print eigenvectors
+    printf("Matrix eigenvectors with %d rows and %d cols.\n", size, size);
+    printMatrix(eigenvectors, size, size, 2);
+
+    // Assign vectors and values to struct
+    Eigen eigen;
+    eigen.eigenValues = eigenvalues;
+    eigen.eigenVectors = eigenvectors;
+
+    return eigen;
+}
+
 double **performPCA(double **data, int rows, int cols)
 {
     // Init covariance matrix;
     double **covMatrix = createCovarianceMatrix(data, rows, cols);
     printf("Covariance matrix covMatrix with %d rows and %d cols.\n", cols, cols);
     printMatrix(covMatrix, cols, cols, 1);
+
+    // Init eigen struct and find eigenvectors and eigenvalues
+    Eigen eigen = findEig(covMatrix, cols);
 }
 
 int main()
