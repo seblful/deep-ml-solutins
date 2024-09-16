ef418763aeb7a17c304104b885ad29ffe0849df0
seblful <alesha.chimba@gmail.com>
2024-09-16 11:10:58 +0300

    [EDIT] add attr struct

--- decision_tree.c
+++ decision_tree.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "utils.h"
 
 #define NUM_ATTRIBUTES 4
 
+typedef struct
+{
+    int id;
+    char name[50];
+    char value[50];
+} attr;
+
+attr createAttr(int id, char *name, char *value)
+{
+    attr newAttribute;
+
+    newAttribute.id = id;
+    strcpy(newAttribute.name, name);
+
+    return newAttribute;
+};
+
 int main()
 {
-    char *attributes[NUM_ATTRIBUTES] = {"Outlook", "Temperature", "Humidity", "Wind"};
-
-    // Print attributes
-    printf("Array attributes with size %d:\n", NUM_ATTRIBUTES);
-    for (int i = 0; i < NUM_ATTRIBUTES; i++)
+    // Init and fill attribute struct
+    char *properties[NUM_ATTRIBUTES] = {"Outlook", "Temperature", "Humidity", "Wind"};
+    attr *attributes = (attr *)malloc(NUM_ATTRIBUTES * sizeof(attr));
+    for (int i; i < NUM_ATTRIBUTES; i++)
     {
-        printf("%s\n", attributes[i]);
-    };
+        attributes[i] = createAttr(i, properties[i], "");
+    }
+
+    // // Print attributes
+    // printf("Array attributes with size %d:\n", NUM_ATTRIBUTES);
+    // for (int i = 0; i < NUM_ATTRIBUTES; i++)
+    // {
+
+    //     printf("Name of attr: %s.\n", attributes[i].name);
+    // };
 }
