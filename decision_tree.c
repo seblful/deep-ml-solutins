eaf399d99ae84744234e9402783545b0da65539e
seblful <alesha.chimba@gmail.com>
2024-09-17 15:01:36 +0300

    [EDIT] add data

--- decision_tree.c
+++ decision_tree.c
@@ -4,13 +4,15 @@
 
 #include "utils.h"
 
+#define MAX_STR_LENGTH 50
+#define DATA_LENGTH 4
 #define NUM_ATTRIBUTES 4
 
 typedef struct
 {
     int id;
-    char name[50];
-    char value[50];
+    char name[MAX_STR_LENGTH];
+    char value[MAX_STR_LENGTH];
 } attr;
 
 attr createAttr(int id, char *name, char *value)
@@ -29,12 +31,36 @@ void printAttr(attr attribute)
     printf("Attribute id: %d, name: '%s', value: '%s'.\n", attribute.id, attribute.name, attribute.value);
 }
 
+float calculateEntropy()
+{
+    ;
+}
+
+float calculateIG()
+{
+    ;
+}
+
+void createDecisionTree()
+{
+    ;
+}
+
 int main()
 {
+    // Init data
+    char data[DATA_LENGTH][NUM_ATTRIBUTES][MAX_STR_LENGTH] = {
+        {"Sunny", "Hot", "High", "Weak"},
+        {"Sunny", "Hot", "High", "Strong"},
+        {"Overcast", "Hot", "High", "Weak"},
+        {"Rain", "Mild", "High", "Weak"}};
+
+    uint8_t results[DATA_LENGTH] = {0, 0, 1, 1};
+
     // Init and fill attribute struct
-    char *properties[NUM_ATTRIBUTES] = {"Outlook", "Temperature", "Humidity", "Wind"};
+    char properties[NUM_ATTRIBUTES][MAX_STR_LENGTH] = {"Outlook", "Temperature", "Humidity", "Wind"};
     attr *attributes = (attr *)malloc(NUM_ATTRIBUTES * sizeof(attr));
-    for (int i; i < NUM_ATTRIBUTES; i++)
+    for (int i = 0; i < NUM_ATTRIBUTES; i++)
     {
         attributes[i] = createAttr(i, properties[i], "");
     }
