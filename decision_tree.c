6eda46933cfc9d45f052e7fef1efd4dafc6c59dc
seblful <alesha.chimba@gmail.com>
2024-09-25 11:59:12 +0300

    [EDIT] calculate information gain

--- decision_tree.c
+++ decision_tree.c
@@ -7,9 +7,10 @@
 #include "utils.h"
 
 #define MAX_STR_LENGTH 50
-#define DATA_LENGTH 4
+#define DATA_LENGTH 14
 #define NUM_ATTRIBUTES 4
 #define NUM_RESULTS 2
+#define SET_LENGTH 3
 
 typedef struct
 {
@@ -34,8 +35,16 @@ void printAttr(attr attribute)
     printf("Attribute id: %d, name: '%s', value: '%s'.\n", attribute.id, attribute.name, attribute.value);
 }
 
-float calculateEntropy(uint8_t *results, int n)
+double calculateEntropy(uint8_t *results, int n)
 {
+    // // print
+    // printf("word results: ");
+    // for (int i = 0; i < n; i++)
+    // {
+    //     printf("%d ", results[i]);
+    // };
+    // printf("\n");
+
     // Find number of positive and negative answers
     int counters[2] = {0, 0};
 
@@ -57,16 +66,52 @@ float calculateEntropy(uint8_t *results, int n)
     for (int i = 0; i < 2; i++)
     {
         p = counters[i] / (double)n;
-        entropy += -(p * log2(p));
+        if (p != 0)
+        {
+            entropy += -(p * log2(p));
+        }
     }
+
+    return entropy;
 }
 
-float calculateIG()
+double calculateIG(char data[DATA_LENGTH][NUM_ATTRIBUTES][MAX_STR_LENGTH],
+                   uint8_t results[DATA_LENGTH],
+                   char wordSet[SET_LENGTH][MAX_STR_LENGTH],
+                   double entropy,
+                   int i)
 {
-    ;
+    double tSum = 0;
+
+    for (int k = 0; k < SET_LENGTH; k++)
+    {
+        if (strcmp(wordSet[k], "") == 0)
+        {
+            break;
+        }
+
+        uint8_t wordResults[DATA_LENGTH];
+        int counter = 0;
+        int n = 0;
+
+        for (int j = 0; j < DATA_LENGTH; j++)
+        {
+            if (strcmp(wordSet[k], data[j][i]) == 0)
+            {
+                wordResults[n] = results[j];
+                n += 1;
+            };
+        };
+
+        double p = (double)n / DATA_LENGTH;
+
+        tSum += p * calculateEntropy(wordResults, n);
+    }
+
+    return entropy - tSum;
 }
 
-void createWordSet(char data[DATA_LENGTH][NUM_ATTRIBUTES][MAX_STR_LENGTH], char wordSet[3][50], int i)
+void createWordSet(char data[DATA_LENGTH][NUM_ATTRIBUTES][MAX_STR_LENGTH], char wordSet[SET_LENGTH][MAX_STR_LENGTH], int i)
 {
     int wordSetInd = 0;
 
@@ -74,7 +119,7 @@ void createWordSet(char data[DATA_LENGTH][NUM_ATTRIBUTES][MAX_STR_LENGTH], char 
     {
         bool isUnique = true;
 
-        for (int k = 0; k < 3; k++)
+        for (int k = 0; k < SET_LENGTH; k++)
         {
 
             if (strcmp(wordSet[k], data[j][i]) == 0)
@@ -95,22 +140,15 @@ void createWordSet(char data[DATA_LENGTH][NUM_ATTRIBUTES][MAX_STR_LENGTH], char 
 void createDecisionTree(char data[DATA_LENGTH][NUM_ATTRIBUTES][MAX_STR_LENGTH], uint8_t results[DATA_LENGTH])
 {
     double infGain;
+    double entropy = calculateEntropy(results, DATA_LENGTH);
 
     for (int i = 0; i < NUM_ATTRIBUTES; i++)
     {
-        char wordSet[3][50] = {"", "", ""};
+        char wordSet[SET_LENGTH][MAX_STR_LENGTH] = {"", "", ""};
         createWordSet(data, wordSet, i);
 
-        for (int k = 0; k < 3; k++)
-        {
-            if (strcmp(wordSet[k], "") != 0)
-            {
-                printf("%s\n", wordSet[k]);
-            }
-        }
-        printf("\n");
-
-        // do smth with set of words
+        infGain = calculateIG(data, results, wordSet, entropy, i);
+        printf("%f\n", infGain);
     }
 }
 
@@ -121,9 +159,19 @@ int main()
         {"Sunny", "Hot", "High", "Weak"},
         {"Sunny", "Hot", "High", "Strong"},
         {"Overcast", "Hot", "High", "Weak"},
-        {"Rain", "Mild", "High", "Weak"}};
+        {"Rain", "Mild", "High", "Weak"},
+        {"Rain", "Cool", "Normal", "Weak"},
+        {"Rain", "Cool", "Normal", "Strong"},
+        {"Overcast", "Cool", "Normal", "Strong"},
+        {"Sunny", "Mild", "High", "Weak"},
+        {"Sunny", "Cool", "Normal", "Weak"},
+        {"Rain", "Mild", "Normal", "Weak"},
+        {"Sunny", "Mild", "Normal", "Strong"},
+        {"Overcast", "Mild", "High", "Strong"},
+        {"Overcast", "Hot", "Normal", "Weak"},
+        {"Rain", "Mild", "High", "Strong"}};
 
-    uint8_t results[DATA_LENGTH] = {0, 0, 1, 1};
+    uint8_t results[DATA_LENGTH] = {0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0};
 
     // Init and fill attribute struct
     char properties[NUM_ATTRIBUTES][MAX_STR_LENGTH] = {"Outlook", "Temperature", "Humidity", "Wind"};
