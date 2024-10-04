d05c70819969849a92d7b337a524fde69bf39830
seblful <alesha.chimba@gmail.com>
2024-10-04 09:04:54 +0300

    [ADD] softmax

---
+++ softmax.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "utils.h"
+
+double *softmax(double *inputs, int n)
+{
+    // Create array of results
+    double *results = (double *)malloc(n * sizeof(double));
+
+    double sum;
+
+    // Find sum of exponents
+    for (int i = 0; i < n; i++)
+    {
+        sum += exp(inputs[i]);
+    };
+
+    // Find probability of each number
+    for (int i = 0; i < n; i++)
+    {
+        results[i] = exp(inputs[i]) / sum;
+    }
+
+    return results;
+}
+
+int main()
+{
+    int n = 3;
+
+    // Init and fill scores
+    double *scores = (double *)malloc(n * sizeof(double));
+
+    scores[0] = 1;
+    scores[1] = 2;
+    scores[2] = 3;
+
+    // Print vector
+    printf("Vector scores with size %d.\n", n);
+    printVector(scores, n, 1);
+
+    // Softmax
+    double *result = softmax(scores, n);
+    printf("Vector result with size %d.\n", n);
+    printVector(result, n, 4);
+
+    return 0;
+}
