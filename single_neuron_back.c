613acb6894e010b115b896415d4d60ca8bc424fa
seblful <alesha.chimba@gmail.com>
2024-10-07 14:50:07 +0300

    [EDIT] backward pass, mse, free memory

--- single_neuron_back.c
+++ single_neuron_back.c
@@ -4,6 +4,11 @@
 
 #include "utils.h"
 
+double square(double x)
+{
+    return x * x;
+}
+
 double sigmoid(double z)
 {
     return 1 / (1 + exp(-z));
@@ -14,25 +19,28 @@ double sigmoidDer(double z)
     return sigmoid(z) * (1 - sigmoid(z));
 }
 
-double square(double x)
+double calculateMSE(double *result, double *labels, int n)
 {
-    return x * x;
+    double sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += square(result[i] - labels[i]);
+    };
+
+    return sum / (double)n;
 }
 
-double calculateDW(double **data, int rows, int cols, double *result, double *labels)
+double calculateDW(double **data, int rows, double *result, double *labels, int j)
 {
-
     double sum = 0;
 
     for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; i < cols; i++)
-        {
-            sum += (sigmoid(result[i]) - labels[i]) * sigmoidDer(result[i]) * data[i][j];
-        }
+        sum += (sigmoid(result[i]) - labels[i]) * (sigmoidDer(result[i]) * data[i][j]);
     }
 
-    return sum * (2 / rows);
+    return sum * (2 / (double)rows);
 }
 
 double calculateDb(double *result, double *labels, int n)
@@ -43,12 +51,11 @@ double calculateDb(double *result, double *labels, int n)
         sum += (sigmoid(result[i]) - labels[i]) * sigmoidDer(result[i]);
     };
 
-    return sum * (2 / n);
+    return sum * (2 / (double)n);
 }
 
 void forwardPass(double **data, int rows, int cols, double *weights, double bias, double *result)
 {
-
     // Find probabilities
     for (int i = 0; i < rows; i++)
     {
@@ -71,15 +78,20 @@ void backwardPass(double **data,
                   double *result,
                   double *labels,
                   double *weights,
-                  double bias,
+                  double *bias,
                   double lr)
 {
+    // Calculate and update weights
     double dW, db;
+    for (int j = 0; j < cols; j++)
+    {
+        dW = calculateDW(data, rows, result, labels, j);
+        weights[j] = weights[j] - dW * lr;
+    }
 
-    dW = calculateDW(data, rows, cols, result, labels);
-    printf("%f\n", dW);
+    // Calculate and update bias
     db = calculateDb(result, labels, rows);
-    printf("%f\n", db);
+    *bias = *bias - db * lr;
 }
 
 double *singleNeuron(double **data,
@@ -91,14 +103,29 @@ double *singleNeuron(double **data,
                      double lr,
                      int epochs)
 {
-    // Create array to store results
+    // Create array to store results and mse
     double *result = (double *)malloc(rows * sizeof(double));
+    double *MSE = (double *)malloc(epochs * sizeof(double));
 
     for (int t = 0; t < epochs; t++)
     {
+        printf("%d/%d epochs.\n", t + 1, epochs);
         forwardPass(data, rows, cols, weights, bias, result);
-        backwardPass(data, rows, cols, result, labels, weights, bias, lr);
+        backwardPass(data, rows, cols, result, labels, weights, &bias, lr);
+        MSE[t] = calculateMSE(result, labels, rows);
     };
+
+    // Print results
+    printf("Vector result with size %d.\n", rows);
+    printVector(result, rows, 4);
+    printf("Vector weights with size %d.\n", cols);
+    printVector(weights, cols, 4);
+    printf("Bias: %f.\n", bias);
+
+    // Free memory
+    free(result);
+
+    return MSE;
 }
 
 int main()
@@ -144,12 +171,18 @@ int main()
     double lr = 0.1;
     int epochs = 2;
 
-    //    // Pass through single neuron
-    double *result = singleNeuron(features, rows, cols, labels, weights, bias, lr, epochs);
+    // Pass through single neuron
+    double *MSE = singleNeuron(features, rows, cols, labels, weights, bias, lr, epochs);
 
-    // // Print result
-    // printf("Vector result with size %d.\n", rows);
-    // printVector(result, rows, 4);
+    // Print result
+    printf("Vector MSE with size %d.\n", epochs);
+    printVector(MSE, epochs, 4);
+
+    // Free memory
+    freeMatrix(features, rows);
+    free(labels);
+    free(weights);
+    free(MSE);
 
     return 0;
 }
