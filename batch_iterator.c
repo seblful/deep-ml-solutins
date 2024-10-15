b322b9c9395d8c6161e785a49c893eb19d9adb2e
seblful <alesha.chimba@gmail.com>
2024-10-15 09:43:28 +0300

    [EDIT] create batches

--- batch_iterator.c
+++ batch_iterator.c
@@ -3,10 +3,56 @@
 
 #include "utils.h"
 
+typedef struct BatchIterator
+{
+    double ***data;
+    double **result;
+    int batchSize;
+    size_t rows;
+    size_t cols;
+} BatchIterator;
+
+BatchIterator createBatchIterator(double **X, double *y, size_t rows, size_t cols, int batchSize)
+{
+    // Calculate number of batches
+    int numBatches = (rows + batchSize - 1) / batchSize;
+    double ***data = allocate3dMatrix(numBatches, batchSize, cols);
+    double **result = allocateMatrix(numBatches, batchSize);
+
+    // Fill data with batches from X
+    for (int i = 0; i < numBatches; i++)
+    {
+        for (int j = 0; j < batchSize; j++)
+        {
+            int index = i * batchSize + j;
+            if (index < rows)
+            {
+                data[i][j] = X[index];
+                result[i][j] = y[index];
+            }
+            else
+            {
+                data[i][j] = NULL;
+                result[i][j] = 0;
+            }
+        }
+    }
+
+    // Fill batch iterator
+    BatchIterator batches;
+    batches.data = data;
+    batches.result = result;
+    batches.batchSize = batchSize;
+    batches.rows = rows;
+    batches.cols = cols;
+
+    return batches;
+}
+
 int main()
 {
     // Init X
-    int rows = 5, cols = 2;
+    size_t rows = 5, cols = 2;
     double init_X[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     double **X = allocateMatrix(rows, cols);
     // Fill x
@@ -32,6 +78,12 @@ int main()
 
     printf("Vector y with size %d.\n", rows);
     printVector(y, rows, 0);
+
+    // Init batch size
+    int batchSize = 2;
+
+    // Create batches
+    BatchIterator batches = createBatchIterator(X, y, rows, cols, batchSize);
 
     return 0;
 }
