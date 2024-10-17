31257122055aedf61986d74bb822a24b8134a12d
seblful <alesha.chimba@gmail.com>
2024-10-17 09:16:42 +0300

    [ADD] init commit

---
+++ correlation_matrix.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils.h"
+
+int main()
+{
+    // Init X
+    size_t rows = 3, cols = 2;
+    double init_X[] = {1, 2, 3, 4, 5, 6};
+    double **X = allocateMatrix(rows, cols);
+    // Fill x
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            X[i][j] = init_X[i * cols + j];
+        };
+    };
+
+    printf("Matrix X with %d rows and %d cols.\n", rows, cols);
+    printMatrix(X, rows, cols, 0);
+}
