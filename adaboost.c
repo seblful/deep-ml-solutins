0d73e208a5e14edef835fc59a345c939d79b69d6
seblful <alesha.chimba@gmail.com>
2024-10-18 14:17:53 +0300

    [EDIT] calculate predictions

--- adaboost.c
+++ adaboost.c
@@ -19,22 +19,8 @@ typedef struct node
     struct node *next;
 } node_t;
 
-void push(node_t *head, double value)
-{
-    node_t *current = head;
-    while (current->next != NULL)
-    {
-        current = current->next;
-    }
-
-    current->next = (node_t *)malloc(sizeof(node_t));
-    current->next->value = value;
-    current->next->next = NULL;
-}
-
 void printList(node_t *head)
 {
-    // head = head->next;
     while (head != NULL)
     {
         printf("%.2f ", head->value);
@@ -79,12 +65,24 @@ node_t *findUniqueValues(double *array, size_t size)
     return head;
 }
 
+double *createOnes(size_t size)
+{
+    double *ones = (double *)malloc(size * sizeof(double));
+
+    for (int i = 0; i < size; i++)
+    {
+        ones[i] = 1;
+    }
+
+    return ones;
+}
+
 Classifier *performAdaboost(double **X, size_t rows, size_t cols, double *y, int nClf)
 {
     // Init weights
-    double *weights = (double *)malloc(nClf * sizeof(double));
+    double *weights = (double *)malloc(rows * sizeof(double));
 
-    for (int i = 0; i < nClf; i++)
+    for (int i = 0; i < rows; i++)
     {
         weights[i] = 1 / (double)rows;
     }
@@ -98,19 +96,101 @@ Classifier *performAdaboost(double **X, size_t rows, size_t cols, double *y, int
     // Iterate through each classifier and find threshold
     for (int i = 0; i < nClf; i++)
     {
-        double minVal = -INFINITY;
+        // Create new classifier
+        Classifier clf = {0};
+
+        // Define minimum error
+        double minError = INFINITY;
 
         for (int j = 0; j < cols; j++)
         {
-
+            // Find unique values
             for (int k = 0; k < rows; k++)
             {
                 feautureJ[k] = X[k][j];
             }
 
             node_t *uniques = findUniqueValues(feautureJ, rows);
-            printList(uniques);
+
+            // Iterating through unique values
+            while (uniques != NULL)
+            {
+                double p = 1;
+
+                // Get prediction
+                double *prediction = (double *)malloc(rows * sizeof(double));
+                for (int k = 0; k < rows; k++)
+                {
+                    if (feautureJ[k] < uniques->value)
+                    {
+                        prediction[k] = -1;
+                    }
+                    else
+                    {
+                        prediction[k] = 1;
+                    }
+                }
+
+                // Calculate error
+                double error = 0;
+
+                for (int i = 0; i < rows; i++)
+                {
+                    if (y[i] != prediction[i])
+                    {
+                        error += weights[i];
+                    }
+                }
+
+                if (error > 0.5)
+                {
+                    error = 1 - error;
+                    p = -1;
+                }
+
+                if (error < minError)
+                {
+                    clf.polarity = p;
+                    clf.threshold = uniques->value;
+                    clf.feautureIndex = j;
+                    minError = error;
+                }
+
+                // Assign uniques to the next for iteration
+                uniques = uniques->next;
+            };
+        };
+
+        // Calculate alpha
+        clf.alpha = 0.5 * log((1 - minError) / (minError + 1e-10));
+
+        // Get final predictions
+        double *predictions = createOnes(rows);
+        for (int i = 0; i < rows; i++)
+        {
+            if (clf.polarity * X[i][(int)clf.feautureIndex] < clf.polarity * clf.threshold)
+            {
+                predictions[i] = -1;
+            };
         }
+
+        // Find sum of weights and update them
+        double weightSum = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            weightSum += weights[i];
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            weights[i] *= exp(-clf.alpha * y[i] * predictions[i]);
+            weights[i] /= weightSum;
+        }
+        printVector(weights, rows, 5);
+        // printf("%f ", clf.alpha);
+        // printf("%f ", exp(-clf.alpha * y[i] * predictions[i]));
+        // printf("SUM W %f\n", weightSum);
+        // printVector(predictions, rows, 2);
     }
 }
 
