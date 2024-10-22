133f60e1668f4f27adf9bc972bfe000cbebd1def
seblful <alesha.chimba@gmail.com>
2024-10-22 12:00:23 +0300

    [EDIT] createTransMatrix

--- translation_matrix.c
+++ translation_matrix.c
@@ -3,6 +3,42 @@
 
 #include "utils.h"
 
+#define SIZE 3
+
+double **createTransMatrix(double tx, double ty)
+{
+    // Allocate memory for matrix
+    double **transMatrix = allocateMatrix(SIZE, SIZE);
+
+    // Fill matrix
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (i == j)
+            {
+                transMatrix[i][j] = 1;
+            }
+            else
+            {
+                transMatrix[i][j] = 0;
+            }
+        }
+    }
+
+    transMatrix[0][2] = tx;
+    transMatrix[1][2] = ty;
+
+    return transMatrix;
+}
+
+double **translateMatrix(double **points, size_t rows, size_t cols, double tx, double ty)
+{
+    // Create translation matrix;
+    double **transMatrix = createTransMatrix(tx, ty);
+    printMatrix(transMatrix, SIZE, SIZE, 2);
+}
+
 int main()
 {
     double tx = 2, ty = 3;
@@ -21,6 +57,9 @@ int main()
 
     printf("Matrix points with %zu rows and %zu cols.\n", rows, cols);
     printMatrix(points, rows, cols, 1);
+
+    // Translate matrix
+    double **result = translateMatrix(points, rows, cols, tx, ty);
 
     return 0;
 }
