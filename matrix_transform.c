7562a42abab42df2f5b24c27eaec19419beddd71
seblful <alesha.chimba@gmail.com>
2024-10-25 09:52:46 +0300

    [EDIT] inverseMatrix

--- matrix_transform.c
+++ matrix_transform.c
@@ -3,6 +3,41 @@
 
 #include "utils.h"
 
+double calculateDeterminant(double **M)
+{
+    return M[0][0] * M[1][1] - M[0][1] * M[1][0];
+}
+
+double **inverseMatrix(double **A, size_t rows, size_t cols)
+{
+
+    double determinant = calculateDeterminant(A);
+
+    if (determinant == 0)
+    {
+        printf("Matrix is singular and cannot be inverted.\n");
+        return NULL;
+    }
+
+    double **A_I = allocateMatrix(rows, cols);
+
+    // Calculate the inverse using the formula
+    A_I[0][0] = A[1][1] / determinant;
+    A_I[0][1] = -A[0][1] / determinant;
+    A_I[1][0] = -A[1][0] / determinant;
+    A_I[1][1] = A[0][0] / determinant;
+
+    return A_I;
+}
+
+double **transformMatrix(double **A, double **T, double **S, size_t rows, size_t cols)
+{
+    if (rows != 2 || cols != 2)
+    {
+        return NULL; // Only handle 2x2 matrices
+    }
+}
+
 int main()
 {
     // Init matrices
@@ -32,11 +67,15 @@ int main()
     printMatrix(S, rows, cols, 0);
 
     // Transform matrix
+    double **transfMatrix = transformMatrix(A, T, S, rows, cols);
+    printf("Matrix transfMatrix with %zu rows and %zu cols.\n", rows, cols);
+    printMatrix(transfMatrix, rows, cols, 0);
 
     // Free memory
     freeMatrix(A, rows);
     freeMatrix(T, rows);
     freeMatrix(S, rows);
+    freeMatrix(transfMatrix, rows);
 
     return 0;
 }
