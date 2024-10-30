c61b04b4b2901ca88666bb4462da3ff09b31e6e3
seblful <alesha.chimba@gmail.com>
2024-10-30 10:53:31 +0300

    [EDIT] OSAMatrix

--- OSA.c
+++ OSA.c
@@ -4,6 +4,24 @@
 
 #include "utils.h"
 
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+int findMin(int a, int b, int c)
+{
+    int min = a;
+
+    if (b < min)
+    {
+        min = b;
+    }
+    if (c < min)
+    {
+        min = c;
+    }
+
+    return min;
+}
+
 int performOSA(char source[], char target[])
 {
     // Allocate zero matrix
@@ -17,6 +35,46 @@ int performOSA(char source[], char target[])
     }
 
     // Fill the first row and first column with index values
+    for (int j = 1; j < targetLen + 1; j++)
+    {
+        OSAMatrix[0][j] = j;
+    }
+
+    for (int i = 1; i < sourceLen + 1; i++)
+    {
+        OSAMatrix[i][0] = i;
+    }
+
+    // Compute the OSA distance
+    for (int i = 1; i < sourceLen + 1; i++)
+    {
+        for (int j = 1; j < targetLen + 1; j++)
+        {
+            int isNotEqual = source[i - 1] != target[j - 1] ? 1 : 0;
+            // 1 if source[i - 1] != target[j - 1] else 0
+            OSAMatrix[i][j] = findMin(
+                OSAMatrix[i - 1, j] + 1,               // Deletion
+                OSAMatrix[i][j - 1] + 1,               // Insertion
+                OSAMatrix[i - 1][j - 1] + isNotEqual); // Substitution
+
+            if (i > 1 & j > 1 & (source[i - 1] == target[j - 2]) & (source[i - 2] == target[j - 1]))
+            {
+                OSAMatrix[i][j] = min(OSAMatrix[i][j], OSAMatrix[i - 2][j - 2] + 1); // Transposition
+            }
+        }
+    }
+
+    // Print OSAMatrix
+    printf("Matrix OSAMatrix with %zu rows and %zu cols.\n", sourceLen + 1, targetLen + 1);
+    printIntMatrix(OSAMatrix, sourceLen + 1, targetLen + 1);
+
+    // Retrieve distance
+    int distance = OSAMatrix[sourceLen][targetLen];
+
+    // Free memory
+    freeMatrix(OSAMatrix, sourceLen + 1);
+
+    return distance;
 }
 
 int main()
