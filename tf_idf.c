762fdf707a5d1ab8b2bd91af86ac26df38468926
seblful <alesha.chimba@gmail.com>
2024-11-04 11:24:52 +0300

    [EDIT] calculateTf

--- tf_idf.c
+++ tf_idf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "utils.h"
 
@@ -8,10 +9,35 @@
 #define WORDS_PER_SENTENCE 6
 #define WORD_LENGTH 10
 
-double calculateTf(char corpus[SENTENCES][WORDS_PER_SENTENCE][WORD_LENGTH],
-                   char query[WORD_LENGTH])
+double *calculateTf(char corpus[SENTENCES][WORDS_PER_SENTENCE][WORD_LENGTH],
+                    char query[WORD_LENGTH])
 {
-    ;
+    // Allocate memory for tf
+    double *tf = (double *)malloc(SENTENCES * sizeof(double));
+
+    for (int i = 0; i < SENTENCES; i++)
+    {
+        int wordCount = 0;
+        int queryCount = 0;
+
+        for (int j = 0; j < WORDS_PER_SENTENCE; j++)
+        {
+
+            if (corpus[i][j][0])
+            {
+                wordCount += 1;
+            }
+
+            if (strcmp(corpus[i][j], query) == 0)
+            {
+                queryCount += 1;
+            };
+        }
+
+        tf[i] = (double)queryCount / (double)wordCount;
+    }
+
+    return tf;
 }
 
 double calculateIdf(char corpus[SENTENCES][WORDS_PER_SENTENCE][WORD_LENGTH],
@@ -20,11 +46,18 @@ double calculateIdf(char corpus[SENTENCES][WORDS_PER_SENTENCE][WORD_LENGTH],
     ;
 }
 
-double calculateTfidf(char corpus[SENTENCES][WORDS_PER_SENTENCE][WORD_LENGTH],
-                      char query[WORD_LENGTH])
+double *calculateTfidf(char corpus[SENTENCES][WORDS_PER_SENTENCE][WORD_LENGTH],
+                       char query[WORD_LENGTH])
 {
-    double tf;
+    double *tf = calculateTf(corpus, query);
+    printf("Vector tf with size %zu.\n", SENTENCES);
+    printVector(tf, SENTENCES, 3);
     double idf;
+
+    // Allocate memory for tfidf
+    double *tfidf = (double *)malloc(SENTENCES * sizeof(double));
+
+    return tfidf;
 }
 
 int main()
@@ -51,9 +84,12 @@ int main()
 
     printf("Query is %s.\n", query);
 
-    double tfidf = calculateTfidf(corpus, query);
+    double *tfidf = calculateTfidf(corpus, query);
 
-    printf("TF-IDF of query %s in corpus is %0.3f.\n", query, tfidf);
+    printf("Vector tfidf with size %zu.\n", SENTENCES);
+    printVector(tfidf, SENTENCES, 3);
+
+    // Free memory
 
     return 0;
 }
