a859e184e8df59ed02be23fbef45810b12e6de80
seblful <alesha.chimba@gmail.com>
2024-11-05 11:10:18 +0300

    [EDIT] createUniqueArray

--- gini_impurity.c
+++ gini_impurity.c
@@ -4,6 +4,59 @@
 
 #include "utils.h"
 
+typedef struct Set
+{
+    double *classes;
+    size_t size;
+} set_t;
+
+bool isUnique(double *a, double x, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        if (a[i] == x)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+set_t createUniqueArray(double *y, size_t size)
+{
+    if (size == 0) // Added check for empty array
+    {
+        return (set_t){NULL, 0};
+    }
+
+    size_t setSize = 0;
+    double *classes = (double *)malloc(size * sizeof(double));
+
+    if (classes == NULL) // Added NULL check
+    {
+        return (set_t){NULL, 0};
+    }
+
+    classes[setSize++] = y[0];
+
+    for (size_t i = 1; i < size; i++)
+    {
+        if (isUnique(classes, y[i], setSize))
+        {
+            classes[setSize++] = y[i];
+        }
+    }
+
+    // Resize to actual size used
+    double *temp = (double *)realloc(classes, setSize * sizeof(double));
+    if (temp != NULL)
+    {
+        classes = temp;
+    }
+
+    return (set_t){classes, setSize};
+}
+
 double giniImpurity(double *y, size_t size)
 {
     set_t classesSet = createUniqueArray(y, size);
