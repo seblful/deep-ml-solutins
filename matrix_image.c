cde9e91ae719bd158b3ce02eda7cec418a09183b
seblful <alesha.chimba@gmail.com>
2024-11-11 10:59:35 +0300

    [EDIT] matrixImage

--- matrix_image.c
+++ matrix_image.c
@@ -5,22 +5,39 @@
 
 typedef struct pivotResult
 {
-    double *data;
+    int *data;
     size_t size;
 } pivotResult;
 
-void rref(double **M, size_t rows, size_t cols)
+typedef struct imageResult
 {
+    double **data;
+    size_t rows;
+    size_t cols;
+} imageResult;
+
+double **rref(double **M, size_t rows, size_t cols)
+{
+    // Make copy of original matrix
+    double **rrefM = allocateMatrix(rows, cols);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            rrefM[i][j] = M[i][j];
+        }
+    }
+
     size_t lead = 0;
 
     for (size_t r = 0; r < rows; r++)
     {
         if (lead >= cols)
-            return;
+            return rrefM;
 
         // Find the pivot row
         size_t i = r;
-        while (M[i][lead] == 0)
+        while (rrefM[i][lead] == 0)
         {
             i++;
             if (i == rows)
@@ -28,20 +45,20 @@ void rref(double **M, size_t rows, size_t cols)
                 i = r;
                 lead++;
                 if (lead == cols)
-                    return;
+                    return rrefM;
             }
         }
 
         // Swap the current row with the pivot row
-        double *temp = M[r];
-        M[r] = M[i];
-        M[i] = temp;
+        double *temp = rrefM[r];
+        rrefM[r] = rrefM[i];
+        rrefM[i] = temp;
 
         // Normalize the pivot row
-        double divisor = M[r][lead];
+        double divisor = rrefM[r][lead];
         for (size_t j = 0; j < cols; j++)
         {
-            M[r][j] /= divisor;
+            rrefM[r][j] /= divisor;
         }
 
         // Eliminate all other entries in this column
@@ -49,21 +66,23 @@ void rref(double **M, size_t rows, size_t cols)
         {
             if (k != r)
             {
-                double factor = M[k][lead];
+                double factor = rrefM[k][lead];
                 for (size_t j = 0; j < cols; j++)
                 {
-                    M[k][j] -= factor * M[r][j];
+                    rrefM[k][j] -= factor * rrefM[r][j];
                 }
             }
         }
 
         lead++;
     }
+
+    return rrefM;
 }
 
 pivotResult findPivotColumns(double **M, int rows, int cols)
 {
-    double *pivotCols = (double *)malloc(cols * sizeof(double));
+    int *pivotCols = (int *)malloc(cols * sizeof(int));
     size_t size = 0;
 
     for (int j = 0; j < cols; j++)
@@ -98,17 +117,36 @@ pivotResult findPivotColumns(double **M, int rows, int cols)
     return result;
 }
 
-double **matrixImage(double **M, size_t rows, size_t cols)
+imageResult matrixImage(double **M, size_t rows, size_t cols)
 {
     // RREF
-    rref(M, rows, cols);
+    double **rrefM = rref(M, rows, cols);
     printf("Matrix RREF M with %zu rows and %zu cols.\n", rows, cols);
-    printMatrix(M, rows, cols, 0);
+    printMatrix(rrefM, rows, cols, 0);
 
     // Find pivot column
-    pivotResult pivots = findPivotColumns(M, rows, cols);
+    pivotResult pivots = findPivotColumns(rrefM, rows, cols);
     printf("Vector pivots with size %zu.\n", pivots.size);
     printVector(pivots.data, pivots.size, 0);
+
+    // Create image matrix
+    double **imageM = allocateMatrix(rows, pivots.size);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < pivots.size; j++)
+        {
+            imageM[i][j] = M[i][pivots.data[j]];
+        }
+    }
+
+    // Fill result
+    imageResult result = {imageM, rows, pivots.size};
+
+    // Free memory
+    freeMatrix(rrefM, rows);
+    free(pivots.data);
+
+    return result;
 }
 
 int main()
@@ -129,9 +167,13 @@ int main()
     printMatrix(M, rows, cols, 0);
 
     // Matrix Image
-    matrixImage(M, rows, cols);
+    imageResult result = matrixImage(M, rows, cols);
+    printf("Matrix imageM with %zu rows and %zu cols.\n", result.rows, result.cols);
+    printMatrix(result.data, result.rows, result.cols, 0);
 
     // Free memory
+    freeMatrix(M, rows);
+    freeMatrix(result.data, result.rows);
 
     return 0;
 }
